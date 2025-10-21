bc271f279ecbd9130bb78f737fd2901701e7d8f9
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2025-10-21 22:16:04 +0100

    Add files via upload

---
+++ supplier.py
@@ -0,0 +1,57 @@
+print("\033c\033[43;30m\n")
+files="supplier.csv"
+def menu():
+    value="""0...add
+1...list
+2...report
+3...exit"""
+    print(value)
+    a=input().strip()
+    return int(a)
+def adds():
+    print("id number?")
+    a=input().replace(",",";")
+    print("name?")
+    b=input().replace(",",";")
+    print("address?")
+    c=input().replace(",",";")
+    print("phone?")
+    d=input().replace(",",";")
+    print("email?")
+    e=input().replace(",",";")
+    print("about?")
+    f=input().replace(",",";")
+    a=a+","+b+","+c+","+d+","+e+","+f+"\n"
+    f1=open(files,"a")
+    f1.write(a)
+    f1.close()
+def lists():
+    f1=open(files,"r")
+    a=f1.read()
+    f1.close()
+    print("\033c\033[43;30m\n")
+    print(a)
+def reports():
+    f1=open(files,"r")
+    a=f1.read()
+    f1.close()
+    b=a.split("\n")
+    a=""
+    print("\033c\033[43;30m\n")
+    print("find wat?")
+    c=input()
+    for d in b:
+        f=d.find(c)
+        if f>-1:
+            print(d)
+w=True
+while w:
+    a=menu()
+    if a==0:
+        adds()
+    if a==1:
+        lists()
+    if a==2:
+        reports()
+    if a>2:
+        break
